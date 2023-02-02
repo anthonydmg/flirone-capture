e7fdb4d7ae5a60f53a56e461fb6fa901e9885e04
anthony <anthonymirandagil@gmail.com>
2023-02-02 15:25:54 -0500

    [update] fixes

--- gps_reciver.py
+++ gps_reciver.py
@@ -1,29 +1,39 @@
 from gps import *
 import time
 
-running = True
 
-class GPS_RECEIVER():
+class GPS_RECEIVER:
     def __init__(self):
         self.gpds = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
         self.connected  = True
-        self.get_current_location()
+        #time.sleep(1.0)
+        #self.get_current_location()
 
     def get_current_location(self):
-        try:
-            if self.connected:
-                nx = self.gpds.next()
-                if nx['class'] == 'TPV':
-                    latitude = getattr(nx,'lat', "Unknown")
-                    longitude = getattr(nx,'lon', "Unknown")
-                    print ("Your position: lon = " + str(longitude) + ", lat = " + str(latitude))
+        #try:
+        if self.connected == True:
+            nx = self.gpds.next()
+            print(nx)
+            if nx['class'] == 'TPV':
+                latitude = getattr(nx,'lat', "Unknown")
+                longitude = getattr(nx,'lon', "Unknown")
+                print ("Your position: lon = " + str(longitude) + ", lat = " + str(latitude))
                 location = { 
                     "latitude" : latitude,
                     "longitude": longitude}
+                
                 return location
-            else: 
-                return {"latitude": "", "longitude": ""}
-        except:
-            print("GPS no conectado..")
-            self.connected = False
             return {"latitude": "", "longitude": ""}
+        else: 
+            return {"latitude": "", "longitude": ""}
+        #except Exception as error:
+        #    print("GPS no conectado..")
+        #    print(error)
+        #    self.connected = False
+        #    return {"latitude": "", "longitude": ""}
+
+
+if __name__ == "__main__":
+    gps_receiver = GPS_RECEIVER()
+    location = gps_receiver.get_current_location()
+    print(location)
