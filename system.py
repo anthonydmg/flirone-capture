71096f499994741b690e21135bc24039c00e4bf2
anthony <anthonymirandagil@gmail.com>
2023-06-06 14:24:30 -0500

    []

--- system.py
+++ system.py
@@ -37,7 +37,7 @@ CURRENT_LOCATION = {"latitude": "", "longitude": ""}
 CURRENT_ALTIMETER_DATA = { 
          "presion": 0,  
          "alture": 0, 
-         "alture_over_sea_level": 0,
+         "altitud_over_sea_level": 0,
          "environment_temperature": 0,
          "alture_lib_adafruit": 0,
          "alture_lib_bm280": 0 
